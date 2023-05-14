7cae6cb35c96841cce8bc7e2425c454c97a22286
yghernan <102157602+saitanax3000@users.noreply.github.com>
2023-05-14 16:20:08 -0400

    cambios, api docs
    Co-Authored-By: diegete <101644060+diegete@users.noreply.github.com>

--- bodega/settings.py
+++ bodega/settings.py
@@ -132,3 +132,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# autorización
+CORS_ALLOWED_ORIGINS = [    
+]
+
+# para añadir la auto documentación de tus apis
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
