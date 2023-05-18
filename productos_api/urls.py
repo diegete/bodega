651b6ee54315d0a99f3a2551ede52d8b55cebede
diegete <di.estay@duocuc.cl>
2023-05-18 09:46:27 -0400

    integracion

--- productos_api/urls.py
+++ productos_api/urls.py
@@ -7,10 +7,12 @@ from productos_api import views
 
 router = routers.DefaultRouter()
 router.register(r'productos', views.ProductoViews,'productos')
-
+#instalar requests 
 urlpatterns = [
     path('', home_view, name='home'),
     path('', include(router.urls)),
     path('docs/', include_docs_urls(title='Api productos')),
+    path('api/respuesta', hola_mundo, name='respuesta'),
+    path('api/user',user,name='usuarios')
 
 ]
